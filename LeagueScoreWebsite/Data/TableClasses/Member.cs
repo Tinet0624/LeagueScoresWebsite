ef4ea08de4e5c3e38d225268473c8fe100356e8f
timothyfox443 <timothyfox443@gmail.com>
2021-11-05 15:07:33 -0700

    created Member table stills needs to be linked to Identiy ID field make sure that DateOnly property is the right one

---
+++ LeagueScoreWebsite/Data/TableClasses/Member.cs
@@ -0,0 +1,22 @@
+﻿namespace LeagueScoreWebsite.Data.TableClasses
+{
+    public class Member
+    {
+        public int MemberId { get; set; } //this is linked to Identity ID field somehow
+
+        public string FName { get; set; }
+
+        public string LName { get; set; }
+
+        public DateOnly DateOfBirth { get; set; } //yy, mm, dd
+
+        public bool IsAdmin { get; set; }
+
+        //public List<BowlingSquad> BowlingSquads { get; set; }
+
+        //this is linked to Rolls?
+
+        
+
+    }
+}
