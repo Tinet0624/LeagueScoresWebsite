f7e2237c819a4c51e1c1e37c207f714da52feb46
timothyfox443 <52527132+timothyfox443@users.noreply.github.com>
2021-11-05 14:50:29 -0700

    created BowlingTournamentStats table (#31)
    linked table to BowlingGameStats table
    modified BowlingGameStats table ID field to reflect primary key
    added comment to BowlingGameStats MemberId field

--- LeagueScoreWebsite/Data/TableClasses/BowlingGameStats.cs
+++ LeagueScoreWebsite/Data/TableClasses/BowlingGameStats.cs
@@ -3,10 +3,10 @@
     public class BowlingGameStats
     {
         
-        //public AspNetUsers MemberID { get; set; }
-        public int BowlingGameID { get; set; }
+        //public AspNetUsers MemberID { get; set; } this is a place holde because nothing links to identity but members
+        public int BowlingGameStatsID { get; set; }
 
-       //public BowlingTournamentStats BowlingTournamentStats { get; set;}
+        public BowlingTournamentStats BowlingTournamentStats { get; set;}
 
         public int BowlingGameScore { get; set; }
 
