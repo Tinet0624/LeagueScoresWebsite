474fd8e30753f0abae433f09248caab811798bfc
timothyfox443 <timothyfox443@gmail.com>
2021-11-05 14:20:57 -0700

    basic framework for the Game table

---
+++ LeagueScoreWebsite/Data/TableClasses/BowlingGameStats.cs
@@ -0,0 +1,15 @@
+﻿namespace LeagueScoreWebsite.Data.TableClasses
+{
+    public class BowlingGameStats
+    {
+        
+        //public AspNetUsers MemberID { get; set; }
+        public int BowlingGameID { get; set; }
+
+       //public BowlingTournamentStats BowlingTournamentStats { get; set;}
+
+        public int BowlingGameScore { get; set; }
+
+        public int PlayerHandicap { get; set; }
+    }
+}
